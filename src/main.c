e2d52af26f7b301869c752e2ba9b7edeedf8d2c9
bombacat <bledion.morina@hotmail.com>
2025-04-20 12:18:51 +0200

    Added BG color, should draw a BG now

--- src/main.c
+++ src/main.c
@@ -4,13 +4,14 @@
 ZFB_Device dev = { "/dev/fb0" };
 
 ZFB_Entity player = {};
+ZFB_Color* color = { 255, 0, 0 };
 
 int main()
 {
 	ZFB_InitFB(&dev);
 	while (true)
 	{
-		ZFB_DrawBG(dev, );
+		ZFB_DrawBG(dev, color, NULL);
 	}
 	return 0;
 }
