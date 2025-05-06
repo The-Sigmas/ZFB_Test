6f399dfc8d6c4dbbe6fca53779f74f04d71a4383
bomba-cat <bledion.morina@hotmail.com>
2025-05-06 17:58:54 +0200

    Windows Implementation

--- src/main.c
+++ src/main.c
@@ -1,17 +1,42 @@
 #include "ZFB.h"
-#include <unistd.h>
 
-ZFB_Device dev = { "/dev/fb0" };
-
-ZFB_Entity player = {};
-ZFB_Color bgColor = { 255, 0, 0 };
-
-int main()
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-	ZFB_InitFB(&dev);
-	while (1)
+	if (msg == WM_DESTROY) PostQuitMessage(0);
+	return DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+	ZFB_Device dev =
 	{
-		ZFB_DrawBG(dev, &bgColor, NULL);
+		.width = 1280,
+		.height = 720
+	};
+
+	ZFB_Entity player = {};
+	ZFB_Color bgColor = { 255, 0, 0 };
+	ZFB_Texture* bgTex = ZFB_LoadTexture("pointer.png");
+
+	ZFB_InitFB(&dev);
+	ZFB_CreateWindow(&dev, hInstance, hPrevInstance, lpCmdLine, nShowCmd);
+
+	MSG msg = {};
+	while (msg.message != WM_QUIT)
+	{
+		while (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+		{
+      TranslateMessage(&msg);
+    	DispatchMessage(&msg);
+    }
+
+		bgColor.r = (bgColor.r + 10) % 255;
+
+		ZFB_DrawBG(dev, NULL, bgTex);
+		ZFB_Present(dev);
+		Sleep(16);
 	}
+
+	ZFB_FreeTextures();
 	return 0;
 }
