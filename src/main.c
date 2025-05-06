ca2629e98040d547eab55b5c9a3a5b8e08bee190
bomba-cat <bledion.morina@hotmail.com>
2025-05-06 19:30:38 +0200

    Added comments (real)

--- src/main.c
+++ src/main.c
@@ -1,17 +1,31 @@
 #include "ZFB.h"
-#include <unistd.h>
-
-ZFB_Device dev = { "/dev/fb0" };
-
-ZFB_Entity player = {};
-ZFB_Color bgColor = { 255, 0, 0 };
 
 int main()
 {
+	// Create the Device and set the Parameters
+	ZFB_Device dev = { "/dev/fb0" };
+	// Initialize the Device
 	ZFB_InitFB(&dev);
+	
+	// Background
+	ZFB_Color bgColor = { 255, 0, 0 };
+
+	// Setup the player
+	ZFB_Entity player = {};
+
+	// Set terminal to raw mode
+	ZFB_RawMode();
 	while (1)
 	{
+		// Print the Debug info
+		// CPU, MEM, PROCMEM
+		ZFB_DInfo();
+		
+
+
+		// Draw the Background
 		ZFB_DrawBG(dev, &bgColor, NULL);
 	}
+	ZFB_ExitRawMode();
 	return 0;
 }
