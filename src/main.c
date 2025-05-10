232485b926aec5dd60084d4c6d6e4f5702601638
bomba-cat <bledion.morina@hotmail.com>
2025-05-10 20:59:52 +0200

    windows is real

--- src/main.c
+++ src/main.c
@@ -1,5 +1,16 @@
 #include "ZFB.h"
 
+/*
+ * This example shows hot to set up a simple
+ * project using the ZFB library/
+ *
+ * NOTE:
+ * - Focused on Windows
+ *   - For Linux see the `master` branch
+ * - Uses WinGDI to create a window
+ *   - for other methods see the respected branches
+ */
+
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	if (msg == WM_DESTROY) PostQuitMessage(0);
@@ -13,13 +24,28 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		.width = 1280,
 		.height = 720
 	};
-
-	ZFB_Entity player = {};
-	ZFB_Color bgColor = { 255, 0, 0 };
-	ZFB_Texture* bgTex = ZFB_LoadTexture("pointer.png");
-
 	ZFB_InitFB(&dev);
 	ZFB_CreateWindow(&dev, hInstance, hPrevInstance, lpCmdLine, nShowCmd);
+
+	ZFB_Color bgColor = { 255, 0, 0 };
+
+	ZFB_Texture* bgTex = ZFB_LoadTexture("pointer.png");
+	ZFB_Texture* playerTex = ZFB_LoadTexture("spaceship.png");
+
+	ZFB_Entity playerEntity =
+	{
+		.id = 0,
+		.physics = // NOTE: you can also define physics seperately, ZFB_PhysicsBody playerPhysics = {...};
+		{
+			.position = { dev.width/2, dev.height/2 }, // ZFB_Vector2
+			.velocity = { 0, 0 },
+			.acceleration = { 0, 0 },
+			.mass = 5,
+			.gravity = true, // TODO: Change to .gravity
+		},
+		.width = 50,
+		.height = 50,	
+	};
 
 	MSG msg = {};
 	while (msg.message != WM_QUIT)
@@ -30,9 +56,18 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     	DispatchMessage(&msg);
     }
 
-		bgColor.r = (bgColor.r + 10) % 255;
-
 		ZFB_DrawBG(dev, NULL, bgTex);
+
+		ZFB_Rect player =
+		{
+			.position = playerEntity.physics.position,
+			.width = playerEntity.width,
+			.height = playerEntity.height,
+			.texture = playerTex // NOTE: Can be NULL aswell
+		};
+
+		ZFB_DrawRect(dev, player, NULL);
+
 		ZFB_Present(dev);
 		Sleep(16);
 	}
