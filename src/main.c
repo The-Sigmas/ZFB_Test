71e2a7adf3fb4c2cfbedbf7de3b94ee6ce6db2d9
bomba-cat <bledion.morina@hotmail.com>
2025-06-01 10:00:06 +0200

    update

--- src/main.c
+++ src/main.c
@@ -11,19 +11,17 @@
  *   - for other methods see the respected branches
  */
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-	if (msg == WM_DESTROY) PostQuitMessage(0);
-	return DefWindowProc(hwnd, msg, wParam, lParam);
-}
-
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
 	ZFB_Device dev =
 	{
 		.width = 1280,
-		.height = 720
+		.height = 720,
+		.title = "ZFB_Test"
 	};
+
+	int bulletCount = 0;
+	ZFB_Entity* bullets = malloc(sizeof(ZFB_Entity) * bulletCount);
 
 	ZFB_Event event = {};
 
@@ -50,8 +48,16 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 			.rotation = 0,
 		},
 		.width = 50,
-		.height = 50,	
+		.height = 50,
 	}; ZFB_Rect player = {.texture = playerTex};
+
+	ZFB_Rect osaka =
+	{
+		.position = { 0, 0 },
+		.width = 200,
+		.height = 200,
+		.texture = osakaTex
+	};
 
 	float rotValue = 0;
 	MSG msg = {};
@@ -59,9 +65,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	{
 		while (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
 		{
-      			TranslateMessage(&msg);
-    			DispatchMessage(&msg);
-    		}
+      		TranslateMessage(&msg);
+    		DispatchMessage(&msg);
+    	}
 
 		rotValue += 0.1;
 
@@ -73,36 +79,76 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		{
 			case ZFB_EVENT_KEYDOWN:
 				{
-					//printf("%d\n", event.data.key.key_code);
 					if(ZFB_IsKeyPressed(39))
 					{
 						ZFB_ApplyTorque(&playerEntity, 0.05);
+					}
+					if(ZFB_IsKeyPressed(38))
+					{
+						ZFB_ApplyForceLocal(&playerEntity, (ZFB_Vector2){0, -0.5});
+					}
+					if(ZFB_IsKeyPressed(40))
+					{
+						ZFB_ApplyForceLocal(&playerEntity, (ZFB_Vector2){0, 0.5});
 					}
 					if(ZFB_IsKeyPressed(37))
 					{
 						ZFB_ApplyTorque(&playerEntity, -0.05);
 					}
+					if(ZFB_IsKeyPressed(34))
+					{
+						dev.width += 100;
+						dev.height += 100;
+						ZFB_UpdateFB(&dev);
+					}
+					if(ZFB_IsKeyPressed(33))
+					{
+						dev.width -= 100;
+						dev.height -= 100;
+						ZFB_UpdateFB(&dev);
+					}
+					if(ZFB_IsKeyPressed(32))
+					{
+						ZFB_Entity bullet =
+						{
+							.physics =
+							{
+								.position = playerEntity.physics.position,
+								.mass = 1,
+								.gravity = false,
+								.rotation = playerEntity.physics.rotation,
+							},
+							.width = 10,
+							.height = 10,
+						};
+						bullets = realloc(bullets, sizeof(ZFB_Entity) * (bulletCount + 1));
+						bullets[bulletCount] = bullet;
+						bulletCount++;
+					}
+					break;
 				}
 		}
 
 		ZFB_DrawBG(dev, NULL, bgTex);
+		//ZFB_DrawBG(dev, &ZFB_Red, NULL);
 
 		ZFB_SyncEntity(&player, playerEntity);
 
 		ZFB_UpdatePhysics(&playerEntity, 1);
 
-		ZFB_Rect osaka =
+		for(int i = 0; i < bulletCount; i++)
 		{
-			.position = { 0, 0 },
-			.width = 200,
-			.height = 200,
-			.rotation = rotValue,
-			.texture = osakaTex
-		};
+			ZFB_Rect bulletRect = {};
+			ZFB_ApplyForceLocal(&bullets[i], (ZFB_Vector2){0, -0.5});
+			ZFB_SyncEntity(&bulletRect, bullets[i]);
+			ZFB_UpdatePhysics(&bullets[i], 1);
+			ZFB_DrawRect(dev, bulletRect, &ZFB_Yellow);
+		}
 
-		ZFB_DrawRect(dev, player, NULL);
+		osaka.rotation = rotValue;
+
+		ZFB_DrawRect(dev, player, &ZFB_Red);
 		ZFB_DrawRect(dev, osaka, NULL);
-		//ZFB_DrawRect(dev, osaka, &ZFB_Red);
 
 		ZFB_Present(dev);
 		Sleep(16);
